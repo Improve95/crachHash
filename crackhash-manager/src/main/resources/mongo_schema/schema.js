b7132910bf048a40dc1c61cb0576a71e1e40985d
e.davydov <davidoveg@gmail.com>
2026-03-16 15:17:49 +0700

    mongo replication complete

--- crackhash-manager/src/main/resources/mongo_schema/schema.js
+++ crackhash-manager/src/main/resources/mongo_schema/schema.js
@@ -1,11 +1,12 @@
 db = connect('mongodb://mongo_user:mongo_password@crackhash_db_mongo:27017/myapp?authSource=admin');
+'mongodb://mongo_user:mongo_password@localhost:27017/myapp?authSource=admin'
+rs.initiate({_id:'rs0',members:[{_id:0,host:'mongo-1:27017',priority:1},{_id:1,host:'mongo-2:27017',priority:0.7},{_id:2,host:'mongo-3:27017',priority:0.5}]})
 
-print("Running migrations...");
-
-db.crack_hash_tasks.insertOne({
-    _id: "user-alice-001",
-    name: "Alice",
-    email: "alice@example.com"
+rs.initiate({
+    _id: "rs0",
+    members: [
+        { _id: 0, host: "mongo-1:27017" },
+        { _id: 1, host: "mongo-2:27017" },
+        { _id: 2, host: "mongo-3:27017" }
+    ]
 })
-
-print("Migrations finished.");
